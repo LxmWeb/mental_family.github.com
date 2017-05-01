3063779ba192866b1e0633b2aa56aaf995c6350f
LxmWeb <1171954457@qq.com>
2017-05-01 20:49:16 +0800

    Add files via upload

---
+++ php20170501/chatTest.php
@@ -0,0 +1,13 @@
+<?php
+/*TestGuest Version 1.0
+*==============================
+*Copy 2016 
+*Web:http://www.com
+*==============================
+*Author: LXM
+*Date: 2017年5月1日
+*/
+define('IN_TG', true);
+
+
+?>
